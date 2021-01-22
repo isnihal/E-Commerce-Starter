b542fc2e1543bfe3c131c3a7b654554d0036fd49
Nihal Ismail <nihalismailk@gmail.com>
2021-01-22 12:48:21 +0300

    Minor refactoring

--- lib/providers/store_provider.dart
+++ lib/providers/store_provider.dart
@@ -9,6 +9,7 @@ import 'package:e_commerce_starter/values.dart' as values;
 class StoreProvider with ChangeNotifier{
 
   //####Section#### {Inventory functions}
+
   List<Product> _products = [
     //TODO: This should be empty when fetching data from the API
     Product(
@@ -43,7 +44,6 @@ class StoreProvider with ChangeNotifier{
         discount: 0,
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     ),
-
   ];
 
   //This is a secure approach to retrieve the product inventory
@@ -51,7 +51,108 @@ class StoreProvider with ChangeNotifier{
     return [..._products];
   }
 
+  //####Section#### {Network functions}
+
+  ////####Section#### {Cart Management Functions}
+
+  List<Product> _cart = [];
+  List<Product> get cart{
+    return[..._cart];
+  }
+
+  List<Product> _wishlist  = [];
+  List<Product> get wishlist{
+    return[..._wishlist];
+  }
+
+  void addToCart(Product product){
+    if(!_cart.contains(product)) _cart.add(product);
+    notifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    _cart.remove(product);
+    notifyListeners();
+  }
+
+  void addToWishList(Product product){
+    _wishlist.add(product);
+  }
+
+  void removeFromWishList(Product product){
+    _wishlist.remove(product);
+    notifyListeners();
+  }
+
+  bool isProductInCart(Product product){
+
+    //Check if a product is present in a cart
+
+    for(int i=0;i<_cart.length;i++){
+      Product obj = _cart[i];
+      if(product.id == obj.name){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool isProductInWishlist(Product product){
+
+    //Check if a product is present in the wishlist
+
+    for(int i=0;i<_wishlist.length;i++){
+      Product obj = _wishlist[i];
+      if(product.name == obj.name){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  ////####Section#### {Accounting Functions}
+
+  double get totalSum {
+
+    //Get the sum of prices of all products
+
+    if (_cart.isEmpty)
+      return 0;
+    else {
+      double sum = 0;
+      _cart.forEach((product) {
+        sum += product.price;
+      });
+      return sum;
+    }
+  }
+
+  double get shippingFee{
+
+    //Define shipping fee in this function
+
+    if(_cart.isEmpty) return 0;
+    else return totalSum * values.SHIPPING_FEE;
+  }
+
+  double get tax{
+
+    //Calculate taxes in this function
+
+    if(_cart.isEmpty) return 0;
+    else return totalSum  * values.TAXES;
+  }
+
+  double get totalAmount{
+
+    //Get the total price including taxes and shipping fee
+
+    if(_cart.isEmpty) return 0;
+    else return totalSum+tax+shippingFee;
+  }
+
   //####Section#### {UI Functions}
+
   Product selectedProduct;
 
   //If brand/category filters of product is involved
@@ -112,99 +213,4 @@ class StoreProvider with ChangeNotifier{
     notifyListeners();
   }
 
-  ////####Section#### {Accounting Functions}
-  double get totalSum {
-
-    //Get the sum of prices of all products
-
-    if (_cart.isEmpty)
-      return 0;
-    else {
-      double sum = 0;
-      _cart.forEach((product) {
-        sum += product.price;
-      });
-      return sum;
-    }
-  }
-
-  double get shippingFee{
-
-    //Define shipping fee in this function
-
-    if(_cart.isEmpty) return 0;
-    else return totalSum * values.SHIPPING_FEE;
-  }
-
-  double get tax{
-
-    //Calculate taxes in this function
-
-    if(_cart.isEmpty) return 0;
-    else return totalSum  * values.TAXES;
-  }
-
-  double get totalAmount{
-
-    //Get the total price including taxes and shipping fee
-
-    if(_cart.isEmpty) return 0;
-    else return totalSum+tax+shippingFee;
-  }
-
-  ////####Section#### {Cart Managment Functions}
-  List<Product> _cart = [];
-  List<Product> get cart{
-    return[..._cart];
-  }
-
-  List<Product> _wishlist  = [];
-  List<Product> get wishlist{
-    return[..._wishlist];
-  }
-
-  void addToCart(Product product){
-    if(!_cart.contains(product)) _cart.add(product);
-    notifyListeners();
-  }
-
-  void removeFromCart(Product product) {
-    _cart.remove(product);
-    notifyListeners();
-  }
-
-  void addToWishList(Product product){
-    _wishlist.add(product);
-  }
-
-  void removeFromWishList(Product product){
-    _wishlist.remove(product);
-    notifyListeners();
-  }
-
-  bool isProductInCart(Product product){
-
-    //Check if a product is present in a cart
-
-    for(int i=0;i<_cart.length;i++){
-      Product obj = _cart[i];
-      if(product.id == obj.name){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool isProductInWishlist(Product product){
-
-    //Check if a product is present in the wishlist
-
-    for(int i=0;i<_wishlist.length;i++){
-      Product obj = _wishlist[i];
-      if(product.name == obj.name){
-        return true;
-      }
-    }
-    return false;
-  }
 }
